9cbefee922098341fe21563207319cc9cfeb3fc5
Hiroyuki NAKAMURA <tel2sip@gmail.com>
2019-02-10 11:25:42 +0900

    rec_nhk: refactoring
    * use argparse package for pasing parameter
    * add retry when acess to url
    * add "User-Agent" header as request
    * --timing parameter is added. this parameter can specifies
      'previous', 'following', 'present', which metadata of program
      can be added to id3tag.
    * when ffmpeg launched, "-err_detect aggressive" added to be sure to
      recording under poor intenet connection.
    * if 'program_logo' exists in program metadata, it can be added to
      id3tag.

--- python/rec_nhk.py
+++ python/rec_nhk.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python
 # coding: utf-8
+import argparse
 import sys
 import subprocess
 import urllib2
 import json
 import xml.etree.ElementTree as ET
+from retry import retry
 from datetime import datetime as DT
 from mutagen.easyid3 import EasyID3
 from mutagen.mp3 import MP3
 from mutagen.id3 import ID3, APIC, error
+
+@retry(tries=4, delay=2, backoff=2)
+def urlopen_w_retry( url ):
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0",}
+    request = urllib2.Request(url, headers=headers)
+    resp = urllib2.urlopen( request ).read()
+    return( resp )
 
 # show id3 tag
 def show_id3_tags(file_path):
@@ -16,17 +25,50 @@ def show_id3_tags(file_path):
     print(tags.pprint())
 
 if __name__ == '__main__':
+    parser=argparse.ArgumentParser( description='Recording NHK radio.' )
+    parser.add_argument('channel', \
+                metavar='channel', \
+                help=' Channel Name' )
+    parser.add_argument('duration', \
+                metavar='duration', \
+                type=int, \
+                help='Duration(minutes)' )
+    parser.add_argument('outputdir', \
+                metavar='outputdir', \
+                nargs='?', \
+                default='.' , \
+                help='Output path default:\'.\'' )
+    parser.add_argument('prefix', \
+                metavar='Prefix name',\
+                nargs='?', \
+                help='Prefix name for output file.' )
+    parser.add_argument('--timing', \
+                nargs='?', \
+                choices=['previous', 'following', 'present'], \
+                default='present')
+    args = parser.parse_args()
+    channel=args.channel
+    duration=args.duration * 60
+    outdir=args.outputdir
+    timing=args.timing
+
+    if args.prefix is None:
+        prefix=args.channel
+    else:
+        prefix=args.prefix
+
     # tools
-    ffmpeg_cmd = '/usr/bin/ffmpeg -loglevel quiet -y -i {} -t {} -acodec libmp3lame -ab 128k {}/{}_{}.mp3'
+    ffmpeg_cmd = '/usr/bin/ffmpeg ' + \
+            '-loglevel quiet -y ' + \
+            '-re -y -err_detect aggressive ' + \
+            '-i {} -t {} -acodec libmp3lame -ab 128k ' + \
+            '{}/{}_{}.mp3'
     path = '{}/{}_{}.mp3'
-    #path = 'ME_2018-02-17-L38-1.mp3'
     # where are you?
     here = 'tokyo'
     area_code = '130'
-    outdir = '.'
-    prefix = ''
     # variables for xml parsing
-    url = 'http://www.nhk.or.jp/radio/config/config_web.xml'
+    url = 'https://www.nhk.or.jp/radio/config/config_web.xml'
     nhk_xpath_base = './/stream_url/data/*'
     nhk_xpath = {
         'NHK1':	'.//stream_url/data/r1hls',
@@ -47,44 +89,19 @@ if __name__ == '__main__':
         'NHK2': u'NHKラジオ第二',
         'FM': u'NHK-FM'
     }
-    timing = 'present'
-
-    # Parameter chech
-    args = sys.argv
-    if len(args) == 1:
-        print( 
-        'usage : ' + args[0] + 
-        ' channel_name duration(minuites) [outputdir] [prefix]' )
-        sys.exit(1)
-
-    if len(args) >= 2:
-        channel=args[1]
-        prefix=channel
-        xpath = nhk_xpath.get( channel , None )
-        if xpath is None:
-            print( "channel doesn't exist" )
-            sys.exit(1)
-        else:
-	    code = nhk_code.get( channel , None )
-
-    if len(args) >= 3 and args[2].isdigit():
-        duration=int( args[2] ) * 60
-    else:
-        print "duration must be digit."
-        sys.exit(1)
-
-    if len(args) >= 4:
-        outdir=args[3]
-
-    if len(args) >= 5:
-        prefix=args[4]
 
     # setting date
     date = DT.now()
     date = date.strftime('%Y-%m-%d-%H_%M')
 
-    resp = urllib2.urlopen( url ).read()
-    root = ET.fromstring( resp )
+    # retrieve download url form xml
+    root = ET.fromstring( urlopen_w_retry( url ) )
+    xpath = nhk_xpath.get( channel , None )
+    if xpath is None:
+        print( "channel doesn't exist" )
+        sys.exit(1)
+    else:
+        code = nhk_code.get( channel , None )
 
     for child in root.findall( nhk_xpath_base ):
         if child.tag == 'area' and child.text == here:
@@ -95,20 +112,19 @@ if __name__ == '__main__':
 
     # get program id
     now_program = json.loads(
-    urllib2.urlopen(now_url).read())['nowonair_list'][code][timing]
+    urlopen_w_retry(now_url))['nowonair_list'][code][timing]
     program_id = now_program['id']
-
-    # ProgramInfo API
-    info_url = info_base.format( area_code, code , program_id, api_key)
-
-    # get program information
-    program = json.loads(urllib2.urlopen(info_url).read())['list'][code]
 
     # Recording...
     cmd = ffmpeg_cmd.format( dl_url, duration, outdir, prefix, date )
     path = path.format( outdir, prefix, date )
     proc = subprocess.call( cmd .strip().split(" ") )
 
+    # ProgramInfo API
+    info_url = info_base.format( area_code, code , program_id, api_key)
+
+    # get program information
+    program = json.loads(urlopen_w_retry(info_url))['list'][code]
     tags = EasyID3(path)
     tags['album'] = nhk_album.get( channel , None )
     if program[0]['title']:
@@ -120,15 +136,25 @@ if __name__ == '__main__':
 
     tags.save()
 
-    logo_url = 'http:' + program[0]['service']['logo_l']['url']
-    coverart = urllib2.urlopen(logo_url).read()
-    audio = MP3(path)
-    audio.tags.add(
-            APIC( encoding=3, # 3 is for utf-8 
-                mime='image/png', # image/jpeg or image/png 
-                type=3, # 3 is for the cover image 
-                desc=u'Cover', 
-                data=coverart))
-    audio.save()
+    logo = program[0]['program_logo']
+    if logo is None:
+        logo = program[0]['service']['logo_l']
+    elif logo is None:
+        logo = program[0]['service']['logo_m']
+    elif logo is None:
+        logo = program[0]['service']['logo_s']
+
+    if logo is not None:
+        logo_url = 'https:' + logo['url']
+        coverart = urlopen_w_retry(logo_url)
+        audio = MP3(path)
+        audio.tags.add(
+             APIC( encoding=3,      # 3 is for utf-8 
+                 mime='image/png',  # image/jpeg or image/png 
+                    type=3,         # 3 is for the cover image 
+                    desc=u'Cover', 
+                    data=coverart))
+        audio.save()
+
     #show_id3_tags(path)
 
